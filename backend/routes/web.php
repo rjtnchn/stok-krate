fe2c16952d7a7b85b7055f22ba6f0fcf7d02a16a
Artemiaa23 <dadulla.shanemarian@gmail.com>
2026-08-23 11:03:04 +0800

    Sprint1

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+require __DIR__.'/auth.php';
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
 });
